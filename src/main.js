07521f948defa72c3ffa3868709a63d8a8b940d0
Bohdan Miskov <bohdanmiskov07@gmail.com>
2025-02-16 19:24:38 +0200

    Add small changes in faq script

--- src/main.js
+++ src/main.js
@@ -1,23 +1,23 @@
 /* #region faq question script */
-document.addEventListener('DOMContentLoaded', () => {
-  const buttons = document.querySelectorAll('.faq-item-btn');
 
-  buttons.forEach(button => {
-    button.addEventListener('click', () => ShowHideAnswer(button));
-  });
+const buttons = document.querySelectorAll('.faq-item-btn');
 
-  function ShowHideAnswer(button) {
-    const parentLi = button.closest('.faq-item');
-    const content = parentLi.querySelector('.faq-item-answer-text');
-    content.style.maxHeight = 0 + 'px';
-
-    if (parentLi) {
-      parentLi.classList.toggle('active');
-    }
-
-    if (parentLi.classList.contains('active')) {
-      content.style.maxHeight = content.scrollHeight + 'px';
-    }
-  }
+buttons.forEach(button => {
+  button.addEventListener('click', () => ShowHideAnswer(button));
 });
+
+function ShowHideAnswer(button) {
+  const parentLi = button.closest('.faq-item');
+  const content = parentLi.querySelector('.faq-item-answer-text');
+  content.style.maxHeight = 0 + 'px';
+
+  if (parentLi) {
+    parentLi.classList.toggle('active');
+  }
+
+  if (parentLi.classList.contains('active')) {
+    content.style.maxHeight = content.scrollHeight + 'px';
+  }
+}
+
 /* #endregion */
